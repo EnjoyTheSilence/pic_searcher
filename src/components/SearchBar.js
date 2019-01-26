8fa41f04d050a6e5b2db2add448d5cb91408fdd4
Sswingle <seiji.swingl@gmail.com>
2019-01-25 18:22:41 -0800

    prevents default search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-
 state = {term:''};
+
+
   //do not put parens around callback function when we do not want the function
   //to be run when app renders
+onFormSubmit = (event) => {
+  event.preventDefault();
+  console.log(this.state.term)
+}
+//arrow function automatically binds this to instance of search bar
+  //can also use this.onFormSubmit = this.onFormSubmit.bind(this) in constructor
 
   render(){
     return (
       <div className="ui segment" >
-        <form className="ui form" >
+        <form onSubmit={this.onFormSubmit} className="ui form" >
           <div className="field" >
             <label> Image Search </label>
-            <input type="text" value={this.state.term} onChange={ (e) => this.setState({term: e.target.value}) }/>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={ (e) => this.setState({term: e.target.value}) }
+            />
           </div>
         </form>
       </div>
