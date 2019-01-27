56f53702cba50bedcd985f24253a0c6bfafb6b2d
Sswingle <seiji.swingl@gmail.com>
2019-01-26 16:10:40 -0800

    added styling in imagecard component and now displays images with correct spacing

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props){
+    super()
+    this.state ={
+      spans:0
+    }
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount(){
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10 + 1);
+
+    this.setState({spans})
+  }
+
   render () {
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img
+        ref={this.imageRef}
         alt={this.props.image.description}
         src={this.props.image.urls.regular}
         />
